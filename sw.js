2193b0bdf28a1413f2bce6b8c803162f921fdae3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-01 02:26:05 +0000

    Deploy thefrvrplaylist/thefrvrplaylist.github.io to github.com/thefrvrplaylist/thefrvrplaylist.github.io.git:master

--- sw.js
+++ sw.js
@@ -26,21 +26,21 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e2218000b81d2c08453a.js"
+    "url": "webpack-runtime-ef0994391229debb5b3a.js"
   },
   {
-    "url": "app-4ef8f440bce3337aacb4.js"
+    "url": "app-125861c3df7602cd48ab.js"
   },
   {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-32c78b429e55eede23f2.js"
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-443b65c3f0ee85da1c77.js"
   },
   {
     "url": "index.html",
-    "revision": "42e1261d31d9b5af06c999c9d9f310f1"
+    "revision": "f5a9c7518d0bdb00b208759553f7b987"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "29bb5257a0ab3a2edbb53dc64e605b00"
+    "revision": "b26825a74cd4c4e5177513ee61c5abd3"
   },
   {
     "url": "2.050f02fd5e58f4d8c4df.css"
@@ -49,30 +49,30 @@ self.__precacheManifest = [
     "url": "component---src-pages-index-jsx.628c23ccc49648caff63.css"
   },
   {
-    "url": "component---src-pages-index-jsx-b1dca24a4f2a5ef8cb34.js"
+    "url": "component---src-pages-index-jsx-b3197cb5df7e689b7bb2.js"
   },
   {
-    "url": "1-8a49c018ebb291017bdc.js"
+    "url": "1-8ed7e95b502d48aed139.js"
   },
   {
-    "url": "0-a34630dc12cf6fcde089.js"
+    "url": "0-c2e286bd97f85a63b6a9.js"
   },
   {
-    "url": "3-d37e4e80299005025071.js"
+    "url": "3-777fab93336d22779fd0.js"
   },
   {
-    "url": "2-3f4f7b066564d5b2d739.js"
+    "url": "2-5ff705c37abd03924653.js"
   },
   {
-    "url": "static/d/613/path---index-6a9-c2q98fKsp2N6EDNWlnpTHeTLVQ.json",
-    "revision": "7ec6e5b414307982169b89e7b7100969"
+    "url": "static/d/800/path---index-6a9-8Z5qsw55rThCUF3ZHtxfRfIsKHU.json",
+    "revision": "5bbee1a3c534d3abfed7d33ec2b40dd0"
   },
   {
-    "url": "component---src-pages-404-jsx-5690a924c2bab2948b21.js"
+    "url": "component---src-pages-404-jsx-523e71573a3a6bd131e9.js"
   },
   {
-    "url": "static/d/400/path---404-html-516-62a-cKg45i9OKiBbMHWFlxAkntPiPA.json",
-    "revision": "4cb438192cee6a9f86c52d03f713537e"
+    "url": "static/d/155/path---404-html-516-62a-efKwbHkjHJ6kPfDzpz1W1Gvb4.json",
+    "revision": "8abf907b276dfbde7f4a22c9fad5ec0f"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
